38c4897dc85e38e55f219fe3b6c85cf26f35d66e
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-08 23:49:54 +0300

    added pid file

--- src/main/java/com/hakaton/blockchain/BlockchainApplication.java
+++ src/main/java/com/hakaton/blockchain/BlockchainApplication.java
@@ -2,12 +2,15 @@ package com.hakaton.blockchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BlockchainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlockchainApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(BlockchainApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 }
