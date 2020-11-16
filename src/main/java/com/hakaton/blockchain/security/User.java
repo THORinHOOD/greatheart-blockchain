1977dd902df542d7bf83af010eaedd1260718c17
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-17 00:06:13 +0300

    fixed User

--- src/main/java/com/hakaton/blockchain/security/User.java
+++ src/main/java/com/hakaton/blockchain/security/User.java
@@ -1,14 +1,45 @@
 package com.hakaton.blockchain.security;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class User {
 
     private String login;
     private String password;
     private List<String> roles;
 
+    public User() {}
+
+    public User(String login, String password, List<String> roles) {
+        this.login = login;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public User setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public User setRoles(List<String> roles) {
+        this.roles = roles;
+        return this;
+    }
 }
